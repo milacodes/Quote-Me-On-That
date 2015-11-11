df077dcef3b993319219343f8a0f72828bd318d3
Mila Wilkinson <miladijah@gmail.com>
2015-11-10 17:23:41 -0800

    added assets folder and new templates

---
+++ application/views/templates/header.php
@@ -0,0 +1,24 @@
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Quote Me On That </title>
+        <!--Import Google Icon Font-->
+        <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <link href='https://fonts.googleapis.com/css?family=Overlock:400italic,700italic' rel='stylesheet' type='text/css'>
+        <!--Import materialize.css-->
+        <link type="text/css" rel="stylesheet" href="<?= base_url('assets/materialize/css/materialize.min.css'); ?>"  media="screen,projection"/>
+        <link type="text/css" rel="stylesheet" href="<?= base_url('assets/materialize/css/style.css'); ?>"  media="screen,projection"/>
+    
+        <!--Let browser know website is optimized for mobile-->
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    
+        <!--Import jQuery before materialize.js-->
+        <script type="text/javascript" src="<?= base_url('assets/materialize/js/jquery.js'); ?>"></script>
+        <script type="text/javascript" src="<?= base_url('assets/materialize/js/materialize.min.js'); ?>"></script>
+    </head>
+    <body>
+        <div class = "valign-wrapper">
+        <p class = "logo valign">"Quote Me On That"</p>
+        <div class="divider"></div>
+        </div>
